3bbcaea5b7e58a596be4f67adfeaf3cc46f7af6f
Mariana <mariana_nborges@outlook.com>
2021-10-15 14:32:08 -0300

    criando pacotes

---
+++ src/mariana/Cliente.java
@@ -0,0 +1,8 @@
+package mariana;
+
+public class Cliente {
+
+    String nome;
+    String cpf;
+    String profissao;
+}
---
+++ src/mariana/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package mariana;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+}
