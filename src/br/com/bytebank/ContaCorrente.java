ba6fdbd89383d319dcdaaf7e75dd4bc2097e78ef
Mariana <mariana_nborges@outlook.com>
2021-10-18 21:15:31 -0300

    adicionando interface

--- src/br/com/bytebank/ContaCorrente.java
+++ src/br/com/bytebank/ContaCorrente.java
@@ -1,6 +1,6 @@
 package br.com.bytebank;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
     public ContaCorrente(int agencia, int numero) {
         super(agencia, numero);
@@ -15,6 +15,11 @@ public class ContaCorrente extends Conta {
     @Override
     public void deposita(double valor) {
         super.saldo +=  valor;
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.saldo * 0.01;
     }
 
 }
---
+++ src/br/com/bytebank/Tributavel.java
@@ -0,0 +1,7 @@
+package br.com.bytebank;
+
+public interface Tributavel {
+
+    double getValorImposto();
+
+}
